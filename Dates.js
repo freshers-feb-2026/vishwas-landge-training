d9b23340a2f9fd6a4b25490ba2a899b792ed9245
vishwas-thinkitive <vishwas.landge@thinkitive.com>
2026-02-26 11:52:24 +0530

    date concepts

--- Dates.js
+++ Dates.js
@@ -2,3 +2,7 @@ const date = new Date();
 console.log(date); // converted to local time automatically
 console.log(date.toISOString()); // stays in UTC
 console.log(date.toLocaleString()); // local timezone
+
+// Timestamp (Milliseconds)
+const now = Date.now();
+console.log(now);
