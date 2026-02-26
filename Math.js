fbeca39b3aac9554f75b0730ae14d5134dd4b38e
vishwas-thinkitive <vishwas.landge@thinkitive.com>
2026-02-26 11:52:41 +0530

    Math lib in js

---
+++ Math.js
@@ -0,0 +1,15 @@
+console.log(Math.round(4.4)); // 4
+console.log(Math.round(4.5)); // 5
+
+console.log(Math.floor(4.9)); // 4
+console.log(Math.floor(-4.2)); // -5
+
+
+console.log(Math.ceil(4.1)); // 5
+console.log(Math.ceil(-4.9)); // -4
+
+console.log(Math.abs(-10)); // 10
+
+
+console.log(Math.min(5, 2, 8)); // 2
+console.log(Math.max(5, 2, 8)); // 8
