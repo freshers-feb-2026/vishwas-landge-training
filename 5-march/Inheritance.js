db4bfd866b5013361f63d72b54ac2736e4d256cd
vishwas-thinkitive <vishwas.landge@thinkitive.com>
2026-03-05 12:32:22 +0530

    Enhance Inheritance.js and ThisKeyword.js with detailed explanations and examples
    - Expanded Inheritance.js to include concepts of single and multiple inheritance, demonstrating Object.create and Object.assign.
    - Added comments on prototype chain and performance considerations.
    - Updated ThisKeyword.js with additional examples of function context manipulation using call, apply, and bind methods, clarifying the behavior of 'this' in different scenarios.

--- 5-march/Inheritance.js
+++ 5-march/Inheritance.js
@@ -16,12 +16,17 @@ let parent = {
 }
 
 
-
+// single inheritance
 let child = Object.create(parent, {
     name: {
     value:"vishwas"
     }
 })
+// is same as  child.__proto__== parent
+// Changing __proto__ after an object is created can slow down JavaScript engines.
+// But Object.create() sets the prototype during object creation, which is faster.
+
+
 
 child.greet();
 console.log(child.__proto__)
@@ -29,3 +34,34 @@ console.log(child.__proto__== parent)
 
 // Inheritance allows an object to use the properties and methods of another object
 //  without duplicating the code.
+
+// Javascripct support inheritance by prototype chain
+
+
+
+// multiple inheritance  (object.assign copies the propery )
+// If two objects have the same property, 
+// the later one overwrites the earlier one.
+const obj1 = {
+    name: "Vishwas",
+    address: {
+      city: "Pune"
+    }
+  };
+
+const obj3={
+ 
+    name:"vishwas landge"
+}  
+  
+  const obj2 = Object.assign({}, obj1, obj3);
+  
+  obj2.address.city = "Mumbai";
+  
+  console.log(obj1.address.city); //mumbai
+
+
+
+
+// Object.assign()	      ==  Copies property references
+// Prototype inheritance  ==  Uses prototype chain lookup
--- 5-march/ThisKeyword.js
+++ 5-march/ThisKeyword.js
@@ -2,7 +2,7 @@
 
 let obj = {
     name: "vishwas",
-    greet() {
+    greet:function(){
         (() => {
             (() => {
                 console.log("This in Arrow Fnc : " ,this.name)
@@ -23,6 +23,14 @@ let obj = {
 
 console.log("Global This : ",this)
 
+console.log("\n====== copy fnc ===\n ")
+
+const copy =obj.greet;
+// You are copying the function reference, not the object context.
+// Because this depends on how the function is called, not where it was defined.
+copy();
+console.log("\n====== fnc ===\n ")
+// the function is called through the object.
 obj.greet();
 obj.greet2();
 
@@ -59,3 +67,58 @@ for (const key in user) {
 // Returns the object automatically
 
 
+
+
+// if you define a function outside and attach it to an object, 
+// calling it through the object will correctly set this to that object.
+// This works because this depends on how the function is called,
+//  not where it was defined.
+
+
+// function greet() {
+//     console.log(this.name);
+//   }
+  
+//   let obj = {
+//     name: "vishwas",
+//     greet: greet
+//   };
+  
+//   obj.greet();
+
+
+
+
+
+// Manipulate this using fnctions 
+
+//// call() runs the function immediately and passes arguments one by one.
+
+function greet(city) {
+    console.log(this.name + " from " + city);
+  }
+  
+  const person = {
+    name: "Vishwas"
+  };
+  
+  greet.call(person, "Pune");
+
+
+  //  apply() also runs the function immediately,
+  //  but arguments are passed as an array.
+
+  const person2 = {
+    name: "Vishwas2"
+  };
+  greet.apply(person2, ["Pune"]);
+
+
+// bind() returns a new function with this fixed.
+
+const person3 = {
+    name: "Vishwas3"
+  };
+  
+const newFunc = greet.bind(person3);
+newFunc("Pune");
