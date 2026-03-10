2b0d878aeb32fba2111e7e9a59f746b48d57fc9b
vishwas-thinkitive <vishwas.landge@thinkitive.com>
2026-03-10 20:37:36 +0530

    delete old code

--- Nodejs/10-march/sequelize/routes/course.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { addCourse, getAllCourse } from "../controllers/course.controller.js";
-
-const router = express.Router();
-
-router.post("/", addCourse);
-router.get("/",getAllCourse)
-
-export default router;
