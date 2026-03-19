88c6cfe28194549d2bb3e7c4cf779ddd83765a3b
vishwas-thinkitive <vishwas.landge@thinkitive.com>
2026-03-19 16:37:56 +0530

    created doctor controller

--- Nodejs/18-march/appointment/routes/doctor.route.js
+++ Nodejs/18-march/appointment/routes/doctor.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import asyncHandler from "../utils/asyncHandler.js";
-import { createAvaibility, getAllAvaibility, getDoctors } from "../controllers/availability.controller.js";
+import { createAvaibility, getAllAvaibility, getDoctors } from "../controllers/doctor.controller.js";
 import isAuth from "../middlewares/isAuth.js";
 import RoleCheck from "../middlewares/RoleCheck.js";
 import { ROLE } from "../constants/index.js";
